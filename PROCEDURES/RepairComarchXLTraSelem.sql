977144fa6b2d68a79bfc66a30c0fb466ac41ea17
Paweł Rokicki <98356463+PR93@users.noreply.github.com>
2023-07-20 10:09:59 +0200

    Update RepairComarchXLTraSelem.sql

--- PROCEDURES/RepairComarchXLTraSelem.sql
+++ PROCEDURES/RepairComarchXLTraSelem.sql
@@ -11,48 +11,44 @@ WHILE @@FETCH_STATUS = 0
    BEGIN 
 
 
-										update cdn.MagSElem set MaS_ZrdLp=null, MaS_SubZrdLp=null where MaS_ZrdNumer=@zrdnumer
-										update cdn.MagNag set MaN_ZrdNumer=@zrdnumer, MaN_ZrdTyp=@zrdtyp, MaN_ZrdFirma=1308673, MaN_ZrdLp=0 where MaN_GIDNumer=@magnumer and MaN_GIDTyp=@magtyp
+	update cdn.MagSElem set MaS_ZrdLp=null, MaS_SubZrdLp=null where MaS_ZrdNumer=@zrdnumer
+	update cdn.MagNag set MaN_ZrdNumer=@zrdnumer, MaN_ZrdTyp=@zrdtyp, MaN_ZrdFirma=1308673, MaN_ZrdLp=0 where MaN_GIDNumer=@magnumer and MaN_GIDTyp=@magtyp
 
-										declare @trsgidlp int
-										declare @trssubgidlp int
-										declare @twrnumer int
-										declare @ilosc decimal(19,4)
+	declare @trsgidlp int
+	declare @trssubgidlp int
+	declare @twrnumer int
+	declare @ilosc decimal(19,4)
 
-										
-										declare kursor2 CURSOR FOR 
-										select TrE_TwrNumer, TrS_Ilosc, TrS_GIDLp, TrS_SubGIDLp from cdn.TraSElem 
-										inner join cdn.TraElem on TrS_GIDNumer=TrE_GIDNumer and TrS_GIDTyp=TrE_GIDTyp and TrS_GIDLp=TrE_GIDLp
-										where TrS_GIDNumer=@zrdnumer 
+	
+	declare kursor2 CURSOR FOR 
+	select TrE_TwrNumer, TrS_Ilosc, TrS_GIDLp, TrS_SubGIDLp from cdn.TraSElem 
+	inner join cdn.TraElem on TrS_GIDNumer=TrE_GIDNumer and TrS_GIDTyp=TrE_GIDTyp and TrS_GIDLp=TrE_GIDLp
+	where TrS_GIDNumer=@zrdnumer 
 
-										open kursor2
+	open kursor2
 
-										FETCH NEXT FROM kursor2 into @twrnumer, @ilosc, @trsgidlp, @trssubgidlp
+	FETCH NEXT FROM kursor2 into @twrnumer, @ilosc, @trsgidlp, @trssubgidlp
 
-										WHILE @@FETCH_STATUS = 0
-										   BEGIN 
-											
-												Update t
-												Set t.MaS_ZrdLp = @trsgidlp, t.MaS_SubZrdLp=@trssubgidlp 
-												From
-												(
-													select top 1 * from cdn.MagSElem where MaS_ZrdNumer=@zrdnumer and MaS_Ilosc=@ilosc and MaS_TwrNumer=@twrnumer 
-														and MaS_ZrdLp is null and MaS_SubZrdLp is null --and MaS_GIDLp=@trsgidlp and MaS_SubGIDLp=@trssubgidlp
-												) t
+	WHILE @@FETCH_STATUS = 0
+	   BEGIN 
+		
+			Update t
+			Set t.MaS_ZrdLp = @trsgidlp, t.MaS_SubZrdLp=@trssubgidlp 
+			From
+			(
+				select top 1 * from cdn.MagSElem where MaS_ZrdNumer=@zrdnumer and MaS_Ilosc=@ilosc and MaS_TwrNumer=@twrnumer 
+					and MaS_ZrdLp is null and MaS_SubZrdLp is null --and MaS_GIDLp=@trsgidlp and MaS_SubGIDLp=@trssubgidlp
+			) t
 
-											  FETCH NEXT FROM kursor2 INTO @twrnumer, @ilosc, @trsgidlp, @trssubgidlp
-										   END 
+		  FETCH NEXT FROM kursor2 INTO @twrnumer, @ilosc, @trsgidlp, @trssubgidlp
+	   END 
 
-										CLOSE kursor2
-										DEALLOCATE kursor2
+	CLOSE kursor2
+	DEALLOCATE kursor2
 
 
-										SET NOCOUNT OFF
-										
-										
-			
-			delete from cdn.tempENSOFT where ZrdNumer=@zrdnumer
-
+	SET NOCOUNT OFF
+					
       FETCH NEXT FROM kursor INTO @zrdnumer
    END 
 
